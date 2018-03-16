afb0551379418cd748aa7e2db9749feeabf0cbbb
Ernesto R. C. Pereda <ercp42@gmail.com>
2018-03-16 16:48:18 -0300

    Create export functions

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-
 import { NativeModules } from 'react-native';
 
 const { IOSWifiManager } = NativeModules;
 
-export default IOSWifiManager;
+console.log(NativeModules);
+
+export const wifiConnect = (ssid) => (
+  IOSWifiManager.connectToSSID(ssid)
+)
+
+export const wifiConnectProtected = (ssid, password, isWep) => (
+  IOSWifiManager.connectToProtectedSSID(ssid, password, isWep)
+)
+
+export const wifiDisconnect = (ssid) => (
+  IOSWifiManager.disconnectFromSSID(ssid)
+)
+
+export const wifiCurrentSSID = () => (
+  IOSWifiManager.currentSSID()
+)
+
+export default {
+  wifiConnect, 
+  wifiConnectProtected,
+  wifiDisconnect,
+  wifiCurrentSSID
+};
